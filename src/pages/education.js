415139166b6ba1287402e4173525fd8d22fa7e88
Branden Phan <branden.phan@gmail.com>
2022-02-17 05:24:24 -0500

    Developing education page

--- src/pages/education.js
+++ src/pages/education.js
@@ -3,10 +3,19 @@ import NavBar from '../components/NavBar'
 import Layout from '../components/Layout'
 import { Grid, Typography } from '@mui/material'
 import guelph from "../images/guelph2.png";
-// import styled from "styled-components";
+import styled from "styled-components";
 // import transcript from "../images/transcript.jpg"
+import javascript from "../images/javascript.svg"
+import html from "../images/html.svg"
+import css from "../images/css.svg"
+import reactImage from "../images/react.svg"
+import gatsbyImage from "../images/gatsby.svg" 
+import graphql from "../images/graphql.svg"
+// import node from "../images/node.svg"
+// import express from "../images/express.svg"
 
 export default function Education() {
+
     return (
         <Layout>
             <div>
@@ -44,141 +53,62 @@ export default function Education() {
                         </Grid>
                     </Grid>
                     <Grid item xs={12}><br /><br /><br /><br /><br /></Grid>
-                    {/* <Grid item xs={12}>
-                        <img src={transcript} alt="" />
-                    </Grid> */}
-                    <Grid item xs={1} />
-                    {/* <Grid item xs={6}>
-                        <div>
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro", color: "#E60268"}}><b>Relevant Coursework</b></Typography>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Software Systems Development and Integration</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2018-2019/courses/cis2750.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>System Analysis and Design in Applications</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2018-2019/courses/cis3750.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>The Analysis and Design of Computer Algorithms</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2018-2019/courses/cis3490.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Structure and Application of Microcomputers</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2014-2015/courses/cis2030.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Software Engineering</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2018-2019/courses/cis3760.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Objected Oriented Programming</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2019-2020/courses/cis2430.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>User Interface Design</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2015-2016/courses/cis2170.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Operating Systems I</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2018-2019/courses/cis3110.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Data Structures</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2018-2019/courses/cis2520.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Parallel Programming</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/~gardnerw/courses/cis3090/index.htm" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Intermediate Programming</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2020-2021/courses/cis2500.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Programming</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2019-2020/courses/cis1300.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Discrete Structures in Computing I</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2017-2018/courses/cis1910.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Discrete Structures in Computing II</b></Typography>
-                            <StyledAnchor href="https://www.uoguelph.ca/registrar/calendars/undergraduate/2019-2020/courses/cis2910.shtml" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Calulus I</b></Typography>
-                            <StyledAnchor href="https://mathstat.uoguelph.ca/node/190" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Statistics I</b></Typography>
-                            <StyledAnchor href="https://mathstat.uoguelph.ca/node/239" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro"}}><b>Linear Algebra I</b></Typography>
-                            <StyledAnchor href="https://mathstat.uoguelph.ca/node/189" target="_blank" rel="noreferrer">
-                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro", color: "#3672FF", marginLeft: "2%"}}>More Details</Typography>
-                            </StyledAnchor>
-                            <br />
-                            <br />
-                        </div>
+                    <Grid item xs={12} sx={{display: "flex", justifyContent: "center"}}>
+                        <Typography variant="h6" sx={{fontFamily: "Source Sans Pro", color: "#E60268"}}><b>Skills</b></Typography>
+                        <br />
+                        <br />
+                        <br />
                     </Grid>
-                    <Grid item xs={5}>
-                        <div>
-                            <Typography variant="h6" sx={{fontFamily: "Source Sans Pro", color: "#E60268"}}><b>Skills</b></Typography>
-                        </div>
-                    </Grid> */}
+
+
+                    <Grid item xs={3} />
+
+                    <Grid item xs={1} sx={{display: "flex", justifyContent: "center"}}>
+                        <StyledDiv>
+                            <img src={html} alt="HTMLLogo" />
+                            <Typography variant="subtitle1" align="center" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;HTML&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+                        </StyledDiv>
+                    </Grid>
+
+                    <Grid item xs={1} sx={{display: "flex", justifyContent: "center"}}>
+                        <StyledDiv>
+                            <img src={css} alt="CSSLogo" />
+                            <Typography variant="subtitle1" align="center" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CSS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+                        </StyledDiv>
+                    </Grid>
+
+                    <Grid item xs={1} sx={{display: "flex", justifyContent: "center"}}>
+                        <StyledDiv>
+                            <img src={javascript} alt="JavaScriptLogo" />
+                            <Typography variant="subtitle1" align="center" sx={{fontFamily: "Source Sans Pro"}}>JavaScript</Typography>
+                        </StyledDiv>
+                    </Grid>
+
+                    <Grid item xs={1} sx={{display: "flex", justifyContent: "center"}}>
+                        <StyledDiv>
+                            <img src={reactImage} alt="ReactLogo" />
+                            <Typography variant="subtitle1" align="center" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;React&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+                        </StyledDiv>
+                    </Grid>
+
+                    <Grid item xs={1} sx={{display: "flex", justifyContent: "center"}}>
+                        <StyledDiv>
+                            <img src={gatsbyImage} alt="GatsbyLogo" />
+                            <Typography variant="subtitle1" align="center" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;&nbsp;&nbsp;Gatsby&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+                        </StyledDiv>
+                    </Grid>
+
+                    <Grid item xs={1} sx={{display: "flex", justifyContent: "center"}}>
+                        <StyledDiv>
+                            <img src={graphql} alt="GraphQLLogo" />
+                            <Typography variant="subtitle1" align="center" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;GraphQL&nbsp;&nbsp;</Typography>
+                        </StyledDiv>
+                    </Grid>
+
+
+                    <Grid item xs={3} />
+
+
                     <Grid item xs={12} sx={{height: "100vh"}} />
 
                 </Grid>
@@ -194,3 +124,10 @@ export default function Education() {
 //         text-decoration: underline;
 //     }
 // `;
+
+const StyledDiv = styled.div`
+    transition: transform .2s;
+    &:hover {
+        transform: scale(1.5);
+    }
+`
