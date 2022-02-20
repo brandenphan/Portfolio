1394935c70b8d739589080b1034dceefcbf2f6c8
Branden Phan <branden.phan@gmail.com>
2022-02-20 05:31:54 -0500

    Final push of the night, move onto code refactoring for about and education before continuing

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -38,54 +38,42 @@ export default function Projects() {
 
 
                     <Grid item xs={12} sx={{display: "flex", justifyContent: "center"}}>
-                        <Grid container sx={{width: "70%", borderRadius: "5px", padding: "1%", border: "2px solid #d7e3fc", boxShadow: "0px 4px 8px 0px #C6D2EC", backgroundColor: "#d7e3fc"}}>
-                            <Grid item xs={0.2} />
+                        <Grid container sx={{width: "70%", borderRadius: "5px",  border: "2px solid #d7e3fc", boxShadow: "0px 4px 8px 0px #C6D2EC", backgroundColor: "#d7e3fc"}}>
                             <Grid item xs={6}>
-                                <img src={lotus} alt="LotusImage" height="100%" width="100%" style={{borderRadius: "5px"}} />
+                                <img src={lotus} alt="LotusImage" height="100%" width="100%" style={{borderRadius: "5px 0px 0px 5px"}} />
                             </Grid>
                             <Grid item xs={0.2} />
                             <Grid item xs={5.6}>
                                 <div>
                                     <br />
+                                    <br />
                                     <Typography align="center" variant="h6" sx={{fontFamily: "Source Sans Pro"}}>
                                         <b>Lotus Thai & Vietnamese Cuisine</b>
                                     </Typography>
-                                    {/* <br /> */}
-                                    <Typography align="center" variant="subtitle1" sx={{fontFamily: "Source Sans Pro"}}>Developed a Web-application to display information pertaining to the Lotus Thai & Vietnamese cuisine restaurant. Written in JavaScript utilizing the React framework. Focus on a modern and responsive user interface while optimizing SEO to increase custom traffic to the web-application.</Typography>
+                                    <Typography align="center" variant="subtitle1" sx={{fontFamily: "Source Sans Pro"}}>Developed a web-application to display information pertaining to the Lotus Thai & Vietnamese cuisine restaurant. Written in JavaScript utilizing the React framework. Emphasis on a modern and responsive user interface while optimizing SEO to increase custom traffic to the web-application.</Typography>
                                     <br />
+                                    <br />
+                                    <br />
+
                                     <Grid container>
-                                        {/* <Grid item xs={12}>
-                                            <Typography align="center" variant="h6" sx={{fontFamily: "Source Sans Pro", color: "#E60268"}}>
-                                                <b>Made with</b>
-                                            </Typography>
-                                            <br />
-                                        </Grid> */}
-                                        <Grid item xs={3} />
-                                        <Grid item xs={2} sx={{display: "flex", justifyContent: "center"}}>
+
+                                        <Grid item xs={2} sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                                             <img src={javascript} alt="JavaScriptLogo" height="70%" width="70%" />
                                         </Grid>
-                                        <Grid item xs={2} sx={{display: "flex", justifyContent: "center"}}>
-                                            <img src={reactImage} alt="ReactLogo" height="70%" width="70%" />
-                                        </Grid>             
-                                        <Grid item xs={2} sx={{display: "flex", justifyContent: "center"}}>
+                                        <Grid item xs={2} sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                                             <img src={node} alt="NodeLogo" height="70%" width="70%" />
                                         </Grid>
-                                        <Grid item xs={3} />
+                                        <Grid item xs={2} sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                            <img src={reactImage} alt="ReactLogo" height="70%" width="70%" />
+                                        </Grid>             
 
-                                        <Grid item xs={3} />
-                                        <Grid item xs={3} sx={{display: "flex", justifyContent: "center"}}>
-                                            <Button sx={{backgroundColor: "#3672FF", "&:hover": {backgroundColor: "#3672FF"}}}>
-                                                <ExitToAppIcon sx={{color: "white", marginTop: "2%"}} />
+                                        <Grid item xs={6} sx={{display: "flex", justifyContent: "flex-end", alignItems: "center"}}>
+                                            <Button sx={{backgroundColor: "#3672FF", "&:hover": {backgroundColor: "#1B5FFF"}, height: "50%"}}>
+                                                <Typography variant="subtitle1">&nbsp;</Typography>
+                                                <ExitToAppIcon sx={{color: "white"}} />
                                                 <Typography variant="subtitle1" sx={{color: "white", fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;Live&nbsp;&nbsp;</Typography>
                                             </Button>
                                         </Grid>
-                                        <Grid item xs={3} sx={{display: "flex", justifyContent: "center"}}>
-                                            <Button sx={{color: "#3672FF", border: "2px solid #3672FF"}}>
-                                                <GitHubIcon sx={{marginTop: "2%"}} />
-                                                <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;Code&nbsp;&nbsp;</Typography>
-                                            </Button>
-                                        </Grid>
-                                        <Grid item xs={3} />
 
                                     </Grid>
                                 </div>
@@ -112,3 +100,13 @@ export default function Projects() {
         </Layout>
     )
 }
+
+
+
+                                        // {/* <Grid item xs={3} sx={{display: "flex", justifyContent: "center"}}>
+                                        //     <Button sx={{color: "#3672FF", border: "2px solid #3672FF"}}>
+                                        //         <Typography variant="subtitle1">&nbsp;</Typography>
+                                        //         <GitHubIcon />
+                                        //         <Typography variant="subtitle1" sx={{fontFamily: "Source Sans Pro"}}>&nbsp;&nbsp;Code&nbsp;&nbsp;</Typography>
+                                        //     </Button>
+                                        // </Grid> */}
